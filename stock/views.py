cd685bddae725853b8cdcf7e5277bef0345e3e5e
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2024-04-10 07:11:57 +0800

    V 2.5.022

--- stock/views.py
+++ stock/views.py
@@ -292,9 +292,9 @@ class StockBinViewSet(viewsets.ModelViewSet):
                 raise APIException({"detail": "Please Enter The Bin Name"})
         for j in range(len(data)):
             current_bin_detail = binset.objects.filter(openid=self.request.auth.openid,
-                                                       bin_name=str(data[j]['bin_name'])).first()
+                                                       bin_name=str(data[j]['bin_name']), is_delete=False).first()
             move_to_bin_detail = binset.objects.filter(openid=self.request.auth.openid,
-                                                       bin_name=str(data[j]['move_to_bin'])).first()
+                                                       bin_name=str(data[j]['move_to_bin']), is_delete=False).first()
             goods_qty_change = stocklist.objects.filter(openid=self.request.auth.openid,
                                                         goods_code=str(data[j]['goods_code'])).first()
             qs_project = qs.filter(t_code=data[j]['t_code']).first()
