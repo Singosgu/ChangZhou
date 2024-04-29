1fddc53c121b7408bb1d0e088188b3f31ac9b6b1
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2024-04-29 19:50:43 +0800

    V 2.5.91

--- dn/views.py
+++ dn/views.py
@@ -841,8 +841,7 @@ class DnOrderReleaseViewSet(viewsets.ModelViewSet):
                                         scanner.objects.create(openid=self.request.auth.openid, mode="PSUM",
                                                                code=dn_detail_list[i].txnid,
                                                                bar_code=Md5.md5(
-                                                                   str(dn_detail_list[i].txnid) + random.randint(1,
-                                                                                                                 9999)))
+                                                                   str(dn_detail_list[i].txnid) + str(random.randint(1, 9999))))
                                     picking_list.append(PickingListModel(openid=self.request.auth.openid,
                                                                          txnid=dn_detail_list[i].txnid,
                                                                          order_line=dn_detail_list[i].order_line,
@@ -937,8 +936,7 @@ class DnOrderReleaseViewSet(viewsets.ModelViewSet):
                                         scanner.objects.create(openid=self.request.auth.openid, mode="PSUM",
                                                                code=dn_detail_list[i].txnid,
                                                                bar_code=Md5.md5(
-                                                                   str(dn_detail_list[i].txnid) + random.randint(1,
-                                                                                                                 9999)))
+                                                                   str(dn_detail_list[i].txnid) + str(random.randint(1,9999))))
                                     picking_list.append(PickingListModel(openid=self.request.auth.openid,
                                                                          txnid=dn_detail_list[i].txnid,
                                                                          order_line=dn_detail_list[i].order_line,
@@ -986,8 +984,7 @@ class DnOrderReleaseViewSet(viewsets.ModelViewSet):
                                         scanner.objects.create(openid=self.request.auth.openid, mode="PSUM",
                                                                code=dn_detail_list[i].txnid,
                                                                bar_code=Md5.md5(
-                                                                   str(dn_detail_list[i].txnid) + random.randint(1,
-                                                                                                                 9999)))
+                                                                   str(dn_detail_list[i].txnid) + str(random.randint(1, 9999))))
                                     picking_list.append(PickingListModel(openid=self.request.auth.openid,
                                                                          txnid=dn_detail_list[i].txnid,
                                                                          order_line=dn_detail_list[i].order_line,
@@ -1054,8 +1051,7 @@ class DnOrderReleaseViewSet(viewsets.ModelViewSet):
                                         scanner.objects.create(openid=self.request.auth.openid, mode="PSUM",
                                                                code=dn_detail_list[i].txnid,
                                                                bar_code=Md5.md5(
-                                                                   str(dn_detail_list[i].txnid) + random.randint(1,
-                                                                                                                 9999)))
+                                                                   str(dn_detail_list[i].txnid) + str(random.randint(1, 9999))))
                                     picking_list.append(PickingListModel(openid=self.request.auth.openid,
                                                                          txnid=dn_detail_list[i].txnid,
                                                                          order_line=dn_detail_list[i].order_line,
@@ -1106,8 +1102,7 @@ class DnOrderReleaseViewSet(viewsets.ModelViewSet):
                                         scanner.objects.create(openid=self.request.auth.openid, mode="PSUM",
                                                                code=dn_detail_list[i].txnid,
                                                                bar_code=Md5.md5(
-                                                                   str(dn_detail_list[i].txnid) + random.randint(1,
-                                                                                                                 9999)))
+                                                                   str(dn_detail_list[i].txnid) + str(random.randint(1,9999))))
                                     picking_list.append(PickingListModel(openid=self.request.auth.openid,
                                                                          txnid=dn_detail_list[i].txnid,
                                                                          order_line=dn_detail_list[i].order_line,
