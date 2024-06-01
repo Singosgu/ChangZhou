912eb798d1075cb9a221b7de8da13c3288978361
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2024-06-01 09:07:47 +0800

    V 2.5.613

--- templates/src/pages/outbound/twopicking.vue
+++ templates/src/pages/outbound/twopicking.vue
@@ -212,6 +212,7 @@ export default {
     getList () {
       var _this = this
       if (_this.$q.localStorage.has('auth')) {
+        getauth('dn/pickinglistfilter/' + '?page=' + this.current, {})
         getauth(_this.pathname + '?page=' + this.current + '&order_line=2', {
         }).then(res => {
           _this.page_count = res.count
