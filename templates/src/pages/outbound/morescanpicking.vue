b96491be982a72bb651a643748e0f8d8b8383842
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2024-06-03 06:25:03 +0800

    V 2.5.803

--- templates/src/pages/outbound/morescanpicking.vue
+++ templates/src/pages/outbound/morescanpicking.vue
@@ -173,14 +173,19 @@ export default {
         if (!res.data.detail) {
           this.resData = res.data.code
           this.resMode = res.data.mode
+          console.log(res.data.mode, this.resMode)
           console.log(res)
           if (this.resMode === 'DN') {
+            console.log(1)
             this.getList(this.resData)
           } else if (this.resMode === 'PSUM') {
+            console.log(2)
             this.PickChange()
           } else if (this.resMode === 'MD') {
+            console.log(3)
             this.MDConfirm(this.resData)
           } else {
+            console.log(4)
             this.$q.notify({
               message: e + '编码不存在',
               icon: 'close',
